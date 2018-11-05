66e3fc838ed2da18d004dcaeee49c8f0220fbb92
Matt Kot <mekot16@gmail.com>
2018-11-04 19:04:06 -0500

    Putting this on GitHub

---
+++ rand-phrase-gen.js
@@ -0,0 +1,17 @@
+'use strict';
+//// DECLARATIONS ////
+const fs = require('fs');
+const nounsData = fs.readFileSync('nouns.json');
+const adjsData = fs.readFileSync('adjectives.json');
+const nouns = JSON.parse(nounsData).nouns;
+const adjs = JSON.parse(adjsData).adjs;
+
+//// HELPER FUNCTIONS ////
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+//// OUTPUT SECTION ////
+let randNoun = capitalizeFirstLetter(nouns[Math.floor(Math.random()*nouns.length)]);
+let randAdj = capitalizeFirstLetter(adjs[Math.floor(Math.random()*adjs.length)]);
+console.log(`${randAdj} ${randNoun}`);
